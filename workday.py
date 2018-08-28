647f9d72e9e14ce5a05fd92bdd8b42d0c665a310
Matthew Armstrong <marmstr93ng@gmai.com>
2018-08-28 22:34:15 +0100

    changed method calls to account for _ addition Added break rule selection

--- workday.py
+++ workday.py
@@ -35,21 +35,31 @@ class WorkDay(object):
         return "{}:{:02d}".format(hour, minute)
 
     def _calc_clk_val(self, clk):
-        hour, minute = self.conv_time_str_to_int(clk)
-        return self.conv_to_min(hour, 'h') + minute
+        hour, minute = self._conv_time_str_to_int(clk)
+        return self._conv_to_min(hour, 'h') + minute
 
     def _calc_pair_time_contrib(self, pair_num):
-        time_clk_in = self.calc_clk_val(self.clockings[pair_num * 2])
-        time_clk_out = self.calc_clk_val(self.clockings[(pair_num * 2) + 1])
+        time_clk_in = self._calc_clk_val(self.clockings[pair_num * 2])
+        time_clk_out = self._calc_clk_val(self.clockings[(pair_num * 2) + 1])
         print("Pair {} IN: {} ({}) OUT: {} ({})".format(pair_num, self.clockings[(pair_num * 2)], time_clk_in, self.clockings[(pair_num * 2) + 1], time_clk_out))
 
         return time_clk_out - time_clk_in
 
-    def _calc_break_time(self):
-        time_clk_out = self.calc_clk_val(self.clockings[-1])
+    def _check_after_two(self):
+        time_clk_out = self._calc_clk_val(self.clockings[-1])
+        if time_clk_out >= self._calc_clk_val("14:00"):
+            return True
+        else:
+            return False
 
-        if time_clk_out >= self.calc_clk_val("14:00"):
-            self.break_time = 45
+    def _calc_break_time(self):
+        if self.break_rule == "rule_1":
+            self.break_time = 15
+            if self._check_after_two():
+                self.break_time += 30
+        elif self.break_time == "rule_2":
+            if self._check_after_two():
+                self.break_time = 45
 
         print("Break Time in minutes: {}".format(self.break_time))
         return self.break_time
@@ -63,7 +73,7 @@ class WorkDay(object):
         else:
             raise ValueError("modification string \'{}\' not supported".format(mod))
 
-        self.total_time = self.conv_time_int_to_str(self.total_time_min)
+        self.total_time = self._conv_time_int_to_str(self.total_time_min)
 
     def calc_day_total_time(self):
         if len(self.clockings) == 0:
@@ -77,20 +87,20 @@ class WorkDay(object):
         clk_pairs = math.ceil(len(self.clockings)/2)
         for pair_num in range(0, clk_pairs):
 
-            pair_time = self.calc_pair_time_contrib(pair_num)
+            pair_time = self._calc_pair_time_contrib(pair_num)
             print("Pair {} time in minutes: {}".format(pair_num, pair_time))
 
-            self.modify_total_time("+", pair_time)
+            self._modify_total_time("+", pair_time)
             print("Current Total Time (min): {} ({})".format(self.total_time, self.total_time_min))
 
-        self.modify_total_time("-", self.calc_break_time())
+        self._modify_total_time("-", self._calc_break_time())
 
 today = WorkDay()
-today.get_break_rule_def()
-#today.add_clocking("9:28")
+#today.get_break_rule_def("rule_2")
+today.add_clocking("9:28")
 #today.add_clocking("12:09")
 #today.add_clocking("13:01")
 #today.add_clocking("13:59")
 #today.add_clocking("16:13")
-#today.calc_day_total_time()
+today.calc_day_total_time()
 print(today.total_time)
