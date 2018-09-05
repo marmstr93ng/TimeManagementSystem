4c3573e16feec8e21a0a709fa7f5a611ef836f75
Matthew Armstrong <marmstr93ng@gmai.com>
2018-09-05 23:42:40 +0100

    Updated the log config to print better to the console

--- tms.py
+++ tms.py
@@ -10,17 +10,17 @@ def main():
     logging.config.fileConfig('logging/log_settings.conf')
     logging.info("Running Time Management System Emulator")
 
-    #today = WorkDay()
+    today = WorkDay()
     #today.get_break_rule_def("rule_2")
-    #today.add_clocking("9:30")
+    today.add_clocking("9:30")
     #today.add_clocking("10:49")
     #today.add_clocking("15:22")
     #today.add_clocking("13:59")
     #today.add_clocking("16:13")
-    #today.calc_day_total_time()
-    #logging.info(today.total_time)
+    today.calc_day_total_time()
+    logging.info(today.total_time)
 
-    scraper()
+    #scraper()
 
 if __name__ == '__main__':
 
