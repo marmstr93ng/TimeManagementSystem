1aa515151d33ae87fa3604760afdd0b16e8a2d67
marmstr93ng <marmstr93ng@gmai.com>
2018-12-14 23:29:13 +0000

    created a new day class to replace the old

--- tests/test_day.py
+++ tests/test_day.py
@@ -1,1 +1,10 @@
 import pytest
+
+from tms import DayNew
+
+class TestDayClass():
+    def setup(self):
+        self.day = DayNew()
+
+    def test_day_class(self):
+        assert self.day.add_clocking() is True
--- tms/__init__.py
+++ tms/__init__.py
@@ -1,5 +1,6 @@
-from tms.day import calc_clk_val, conv_time_int_to_str
+from tms.day_old import calc_clk_val, conv_time_int_to_str
 from tms.breakrule import BreakRule
-from tms.day import Day
+from tms.day_old import Day
+from tms.day import DayNew
 from tms.scraper import scraper
 from tms.week import Week, ctrl_week
